b61c664ae038ac88c66b30a9b6d57fe6f72e773a
Unknown <matqertuk@hotmail.fr>
2018-11-04 01:29:32 +0100

    dd04 update

--- Jeux Vidéo2/dd04.py
+++ Jeux Vidéo2/dd04.py
@@ -2,14 +2,14 @@ import os
 import sys, math, random, pygame
 from pygame.locals import *
 from pygame import *
-#redblobgames
+
 pygame.init()
 
 #Création de la fenetre
 
 largeur = 1024
 hauteur = 768
-# fenetre=pygame.display.set_mode((largeur,hauteur))
+# fenetre = pygame.display.set_mode((largeur,hauteur))
 fenetre=pygame.display.set_mode((largeur,hauteur), RESIZABLE)
 rectFenetre = fenetre.get_rect()
 
@@ -23,7 +23,7 @@ couleur_font = (255,0,0)
 bones = []
 bonestir = []
 tirs=[]
-
+blobs = []
 
 imagesbone = {}
 
@@ -140,10 +140,30 @@ perso["cooldown"]=0
 
 
 
-# creation d'un rectangle pour positioner l'image du personnage
+#dico blob______________________________________________________________________
 rectBlob = imageblob.get_rect()
-rectBlob.x = 240
-rectBlob.y = 120
+blob = {}
+
+
+blob["pv"] = pvblob
+blob["rect"] = rectBlob
+blob["upstairs"] = 0
+blob["nbBlob"] = 2
+
+
+
+
+#_______________________________________________________________________________
+# creation d'un rectangle pour positioner l'image du personnage
+blob["rect"].x = 240
+blob["rect"].y = 120
+blobs.append(blob)
+
+
+
+
+
+
 perso["rect"].x = hauteur/2
 perso["rect"].y = largeur/2
 rectSword = imageSword.get_rect()
@@ -188,7 +208,7 @@ maskPerso = pygame.mask.from_surface(imagePerso)
 maskBlob = pygame.mask.from_surface(imageblob)
 
 # Positionnement de la surface pour le score
-score_surface = score_font.render("Pvblob : {:6d} pv".format(pvblob), 1, couleur_font)
+score_surface = score_font.render("Pvblob : {:6d} pv".format(blob["pv"]), 1, couleur_font)
 score_rect = score_surface.get_rect()
 score_rect.bottomright = rectFenetre.bottomright
 
@@ -198,18 +218,31 @@ score_rect.bottomright = rectFenetre.bottomright
 horloge = pygame.time.Clock()
 
 # la boucle infinie dans laquelle on reste coince
-i=1;
+i=1
 continuer=1
+#perso en haut de la grille = 1 en dessous =0
 upstairsperso = 0
-upstairsblob = 0
+#regule UPSTAIRS
 vartour = -1
+#direction projectile os du PERSO
 dirbone = -1
-Pressed = 0
+#pour plongeon(en attente, garder au cas ou...)
+# Pressed = 0
+#verifie si entité dans l'EAU
 inwater = 0
-hit = 0
-canhit = True
-canshoot = True
-cooldown = 0
+#hp MAX du PERSO
+hpBarMax = 200
+#hp actuels du PERSO
+hp = 200
+#hp PERSO affichés (au cas ou hp < 0, hpAffiche = 0)
+hpAffiche = hp
+#cooldown degats au personnage
+cdmg =50
+#dedouble le blob si il meurt avec nbBlob>1
+dedouble = 0
+newblobx = 0
+newbloby = 0
+newnbBlob = 0
 
 while continuer:
     # print("je peux tirer ? "+str(perso["canshoot"]))
@@ -229,33 +262,17 @@ while continuer:
         inwater = 1
     else : inwater = 0
 #DEPLACE BLOB____________________________________________________________
-    if rectPerso.x > rectBlob.x:
-        rectBlob.x += 5
-    if rectPerso.x < rectBlob.x :
-        rectBlob.x -= 5
-    if rectPerso.y > rectBlob.y:
-        rectBlob.y += 5
-    if rectPerso.y < rectBlob.y:
-        rectBlob.y -= 5
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    for blob in blobs :
+        if rectPerso.x > blob["rect"].x:
+            blob["rect"].x += 5
+        if rectPerso.x < blob["rect"].x :
+            blob["rect"].x -= 5
+        if rectPerso.y > blob["rect"].y:
+            blob["rect"].y += 5
+        if rectPerso.y < blob["rect"].y:
+            blob["rect"].y -= 5
 
 
 
@@ -282,7 +299,7 @@ while continuer:
             if inwater==1 :
                 perso["rect"].y = perso["rect"].y - 5
             else :
-                perso["rect"].y = perso["rect"].y - 8
+                perso["rect"].y = perso["rect"].y - 12
 
     if touches[pygame.K_DOWN] :
         if i%5==0 :
@@ -290,36 +307,21 @@ while continuer:
             imagePerso = imagesPerso["down"][ianime]
         if perso["rect"].y > 616 :
             perso["rect"].y = 617
-        #bloque l escalier droit
-        if perso["rect"].x < 730 and perso["rect"].x >= 680 and perso["rect"].y > 155 and upstairsperso ==1 :
-            perso["rect"].y = 156
-        if perso["rect"].x < 680 and perso["rect"].x >= 630 and perso["rect"].y > 120 and upstairsperso ==1 :
-            perso["rect"].y = 121
-        if perso["rect"].x < 630 and perso["rect"].x >= 580 and perso["rect"].y > 95 and upstairsperso ==1 :
-            perso["rect"].y = 96
-        if perso["rect"].x < 580 and perso["rect"].x >= 530 and perso["rect"].y > 70 and upstairsperso ==1 :
-            perso["rect"].y = 71
-        if perso["rect"].x < 530 and perso["rect"].x >= 480 and perso["rect"].y > 60 and upstairsperso ==1 :
-            perso["rect"].y = 61
-        if perso["rect"].x < 495 and perso["rect"].x >= 440 and perso["rect"].y > 213 and upstairsperso == 1 :
-            perso["rect"].y = 214
-        if perso["rect"].x < 285 and perso["rect"].x >= 0 and perso["rect"].y > 214 and upstairsperso == 1 :
-            perso["rect"].y = 215
+        if perso["rect"].x < 760 and perso["rect"].x >= 480 and perso["rect"].y > 110 and upstairsperso ==1 :
+            perso["rect"].y = 111
+        if perso["rect"].x < 525 and perso["rect"].x >= 0 and perso["rect"].y > 215 and upstairsperso ==1 :
+            perso["rect"].y = 216
         else :
             if inwater==1 :
                 perso["rect"].y = perso["rect"].y + 5
             else :
-                perso["rect"].y = perso["rect"].y + 8
+                perso["rect"].y = perso["rect"].y + 12
+
+
     if touches[pygame.K_LEFT] :
         if i%5==0 :
             ianime = (ianime+1)%len(imagesPerso["left"])
             imagePerso = imagesPerso["left"][ianime]
-        if perso["rect"].x > 700 and perso["rect"].x < 710 and perso["rect"].y > 5 and perso["rect"].y < 200 and upstairsperso==0:
-            perso["rect"].x = 710
-        if perso["rect"].y < 166 and perso["rect"].y >= 156 and perso["rect"].x < 730 and upstairsperso == 1  :
-            perso["rect"].x = 731
-
-
         if perso["rect"].x < 45 and upstairsperso == 0:
            perso["rect"].x = 44
         if perso["rect"].x < 1 and upstairsperso == 1 :
@@ -328,22 +330,25 @@ while continuer:
             if inwater==1 :
                 perso["rect"].x = perso["rect"].x - 5
             else:
-                perso["rect"].x = perso["rect"].x - 8
+                perso["rect"].x = perso["rect"].x - 12
+
+
+
     if touches[pygame.K_RIGHT] :
         if i%5==0 :
             ianime = (ianime+1)%len(imagesPerso["right"])
             imagePerso = imagesPerso["right"][ianime]
-        if perso["rect"].x > 462 and perso["rect"].x < 470 and perso["rect"].y > 70 and upstairsperso==1:
+        if perso["rect"].x > 462 and perso["rect"].x < 475 and perso["rect"].y > 120 and upstairsperso==1:
             perso["rect"].x = 461
         if perso["rect"].x > 918:
             perso["rect"].x = 919
-        if perso["rect"].x > 590 and perso["rect"].x < 600 and perso["rect"].y > -1 and perso["rect"].y < 200 and upstairsperso==0:
+        if perso["rect"].x > 590 and perso["rect"].x < 603 and perso["rect"].y > -1 and perso["rect"].y < 200 and upstairsperso==0:
             perso["rect"].x = 591
         else :
             if inwater==1 :
                 perso["rect"].x = perso["rect"].x + 5
             else:
-                perso["rect"].x = perso["rect"].x + 8
+                perso["rect"].x = perso["rect"].x + 12
 
     # elif touches[pygame.K_SPACE] :
     #     #Affichage de l'épée
@@ -407,11 +412,11 @@ while continuer:
                 t["rect"].y += + 20
         if t["direction"] == 3:
                 t["rect"].x += - 20
-        # if t["rectos"].colliderect(rectBlob) and t["canhit"]:
-        #     t["canhit"] == False
-        #     pvblob -= 5
-        # print("je peux toucher"+str(t[canhit]))
-    # print("nb Bones"+ str(len(bones)))
+
+
+
+
+
     tempTab = []
     for t in tirs:
         if not(t["rect"].x > 1024 or t["rect"].x < 0 or t["rect"].y > 768 or t["rect"].y<0) :
@@ -426,25 +431,16 @@ while continuer:
 
     #affichage os
     #Affichage grille
-                                                                                                                                                                        #or touches[pygame.K_UP] or touches[pygame.K_DOWN]
-    if ((perso["rect"].x >700 and perso["rect"].x < 710) and (perso["rect"].y >-1 and perso["rect"].y < 163) and (vartour+1 < i) and (touches[pygame.K_LEFT] or touches[pygame.K_RIGHT] )):
+    pygame.draw.rect(fenetre, (200, 0,255), pygame.Rect(700, -1,12, 162),0)                                                                                                                                                            #or touches[pygame.K_UP] or touches[pygame.K_DOWN]
+    if ((perso["rect"].x >700 and perso["rect"].x < 713) and (perso["rect"].y >-1 and perso["rect"].y < 163) and (vartour+1 < i) and (touches[pygame.K_LEFT] or touches[pygame.K_RIGHT] )):
         upstairsperso=(upstairsperso+1)%2
         #Pour réguler upstairsperso
         vartour = i
     #changement upstairsblob
-    if ((rectBlob.x >700 and rectBlob.x < 710) and (rectBlob.y >-1 and rectBlob.y < 163) and (vartour+1 < i) and (upstairsblob!=upstairsperso)):
-        upstairsblob=(upstairsblob+1)%2
+    if ((blob["rect"].x >700 and blob["rect"].x < 707) and (blob["rect"].y >-1 and blob["rect"].y < 163) and (vartour+1 < i) and (blob["upstairs"]!=upstairsperso)):
+        blob["upstairs"]=(blob["upstairs"]+1)%2
         vartour = i
 #DEPLACEMENT BLOB TEST 1________________________________________________________
-
-    # if rectbone.colliderect(rectBlob):
-    #     hit += 1
-    #     print(hit)
-    #
-    # if perso["rect"].colliderect(rectBlob):
-    #     hit += 1
-    #     print(hit)
-
 
 
 
@@ -464,21 +460,61 @@ while continuer:
 
     ## Afficher Collision en haut à gauche quand le Perso et le blob ce touche
     # if perso["rect"].colliderect(rectBlob):
-    if maskPerso.overlap(maskBlob, (rectBlob.left - perso["rect"].left, rectBlob.top - perso["rect"].top)) != None:
-        fenetre.blit(imageText,rectText)
+    for blob in blobs :
+        if maskPerso.overlap(maskBlob, (blob["rect"].left - perso["rect"].left, blob["rect"].top - perso["rect"].top)) != None:
+            fenetre.blit(imageText,rectText)
+            if cdmg == 50 :
+                hp -= 10
+                cdmg = 0
+    if cdmg < 50 :
+        cdmg +=1
     #Affiche le score en bas à droite quand le projectil et le blob ce touche
     for t in tirs:
-        if t["rect"].colliderect(rectBlob) :
-            if t["canhit"] == True:
-                pvblob -= 10
-            t["canhit"] = False
+        for blob in blobs :
+            if t["rect"].colliderect(blob["rect"]) :
+                if t["canhit"] == True:
+                    blob["pv"] -= 10
+                    t["canhit"] = False
+    temptablob = []
+    for blob in blobs :
+        if blob["pv"] <= 0 :
+            blob["nbBlob"] -= 1
+            if blob["nbBlob"] >=1:
+                dedouble = 1
+
+    if dedouble == 1 :
+        for blob in blobs:
+            if blob["pv"] <= 0  and blob["nbBlob"] >= 1:
+                newblobx = blob["rect"].x + 20
+                newbloby = blob["rect"].y + 20
+                newnbBlob = blob["nbBlob"]
+        blob["rect"].x = newblobx
+        blob["rect"].y = newbloby
+        blob["nbBlob"] = newnbBlob
+        blob["pv"] = 50
+        temptablob.append(blob)
+
+        blob["rect"].x = newblobx -30
+        blob["rect"].y = newbloby -30
+        blob["nbBlob"] = newnbBlob
+        blob["pv"] = 50
+        temptablob.append(blob)
+        
+        print(str(blobs))
+
+    dedouble = 0
+
+    for blob in blobs :
+        if blob["pv"] > 0 and blob["nbBlob"] >= 1 :
+            temptablob.append(blob)
+    blobs = temptablob
         # fenetre.blit(imageFond, rectFond)
         # display.update(rectBlob)
         #modification du score
     #     score_surface = score_font.render("Score : {:5d} points".format(points), 1, couleur_font)
 
 
-    score_surface = score_font.render("Pvblob : {:5d} Pvblob".format(pvblob), 1, couleur_font)
+    score_surface = score_font.render("Pvblob : {:5d} Pvblob".format(blob["pv"]), 1, couleur_font)
 
 
 
@@ -493,39 +529,63 @@ while continuer:
     #     upstairsblob=(upstairsblob+1)%2
     #     print("upstairsblob= ", upstairsblob)
     # print("upstairsperso= ", upstairsperso)
+#_________________________FONCTIONNEL MAIS LOURD________________________________
+    # if upstairsperso==1 and upstairsblob==1 :
+    #     fenetre.blit(imageGrille, rectGrille)
+    #     fenetre.blit(imagePerso, perso["rect"])
+    #     fenetre.blit(imageblob, rectBlob)
+    #     fenetre.blit(imageHacheDeZoo, rectHacheDeZoo)
+    #     for t in tirs:
+    #         if t["direction"] > -1:
+    #             fenetre.blit(imagebone, t["rect"])
+    # elif upstairsperso==1 and upstairsblob==0:
+    #     fenetre.blit(imageblob, rectBlob)
+    #     fenetre.blit(imageGrille, rectGrille)
+    #     fenetre.blit(imageHacheDeZoo, rectHacheDeZoo)
+    #     fenetre.blit(imagePerso, perso["rect"])
+    #     for t in tirs:
+    #         if t["direction"] > -1:
+    #             fenetre.blit(imagebone, t["rect"])
+    # elif upstairsperso==0 and upstairsblob==0:
+    #     fenetre.blit(imageblob, rectBlob)
+    #     fenetre.blit(imageHacheDeZoo, rectHacheDeZoo)
+    #     fenetre.blit(imagePerso, perso["rect"])
+    #     for t in tirs:
+    #         if t["direction"] > -1:
+    #             fenetre.blit(imagebone, t["rect"])
+    #     fenetre.blit(imageGrille, rectGrille)
+    # elif upstairsperso==0 and upstairsblob==1:
+    #     fenetre.blit(imageHacheDeZoo, rectHacheDeZoo)
+    #     fenetre.blit(imagePerso, perso["rect"])
+    #     for t in tirs:
+    #         if t["direction"] > -1:
+    #             fenetre.blit(imagebone, t["rect"])
+    #     fenetre.blit(imageGrille, rectGrille)
+    #     fenetre.blit(imageblob, rectBlob)
+#_______________________NOUVEL AFFICHAGE________________________________________
+    for blob in blobs :
+        if blob["upstairs"] == 0 :
+            fenetre.blit(imageblob, blob["rect"])
 
-    if upstairsperso==1 and upstairsblob==1 :
-        fenetre.blit(imageGrille, rectGrille)
-        fenetre.blit(imagePerso, perso["rect"])
-        fenetre.blit(imageblob, rectBlob)
-        fenetre.blit(imageHacheDeZoo, rectHacheDeZoo)
-        for t in tirs:
-            if t["direction"] > -1:
-                fenetre.blit(imagebone, t["rect"])
-    elif upstairsperso==1 and upstairsblob==0:
-        fenetre.blit(imageblob, rectBlob)
-        fenetre.blit(imageGrille, rectGrille)
-        fenetre.blit(imageHacheDeZoo, rectHacheDeZoo)
+
+    if upstairsperso == 0 :
         fenetre.blit(imagePerso, perso["rect"])
         for t in tirs:
             if t["direction"] > -1:
                 fenetre.blit(imagebone, t["rect"])
-    elif upstairsperso==0 and upstairsblob==0:
-        fenetre.blit(imageblob, rectBlob)
-        fenetre.blit(imageHacheDeZoo, rectHacheDeZoo)
+
+    fenetre.blit(imageGrille, rectGrille)
+    fenetre.blit(imageHacheDeZoo, rectHacheDeZoo)
+
+    for blob in blobs :
+        if blob["upstairs"] == 1 :
+            fenetre.blit(imageblob, blob["rect"])
+
+    if upstairsperso == 1 :
         fenetre.blit(imagePerso, perso["rect"])
         for t in tirs:
             if t["direction"] > -1:
                 fenetre.blit(imagebone, t["rect"])
-        fenetre.blit(imageGrille, rectGrille)
-    elif upstairsperso==0 and upstairsblob==1:
-        fenetre.blit(imageHacheDeZoo, rectHacheDeZoo)
-        fenetre.blit(imagePerso, perso["rect"])
-        for t in tirs:
-            if t["direction"] > -1:
-                fenetre.blit(imagebone, t["rect"])
-        fenetre.blit(imageGrille, rectGrille)
-        fenetre.blit(imageblob, rectBlob)
 
 
 #plongeon
@@ -565,6 +625,17 @@ while continuer:
     # rafraichissement
     fenetre.blit(imageTree, rectTree)
     fenetre.blit(score_surface, score_rect)
+    #Health Bar contour
+    pygame.draw.rect(fenetre, (200, 0,255), pygame.Rect(largeur-233, hauteur-(hauteur-35), hpBarMax+4, 14),4)
+    #HealthBar
+    if hp < 0 :
+        hpAffiche = 0
+    else :
+        hpAffiche = hp
+    pygame.draw.rect(fenetre, (255, 0, 0), pygame.Rect(largeur-230, hauteur-(hauteur-38), hpAffiche, 10))
+    if hp <= 0 :
+        pygame.quit()
+
     pygame.display.flip()
     #print ( upstairsperso)
 
